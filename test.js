dc4d406f7688360336f55affe8618cf0dbc2d5fe
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-09-27 16:09:47 -0400

    Update test.js

--- test.js
+++ test.js
@@ -4,10 +4,24 @@
   <script>
 		alert('HELLO USER! YOUR COMPUTER IS BEING HACKED! PLEASE HIT THE BLUE BUTTON TO CLEAN YOUR PC.')
 	</script>
+
+<script type = "text/javascript"> 
+	function testing_statement() {
+	document.queryselector("h1").innerHTML = "TEST";
+	document.queryselector("#headingII").innerHTML = "This is a test";
+	document.queryselector(".headingIII").innerHTML = "I repeat, this is a test";
+	document.queryselector("#headingIIII").innerHTML = "<span style ='color:red'> Test test one two check check </span>"
+	
   </head>
 <body>
 	<h3 style = "background-color: blue;" onmouseover="hackerman()"> JS Test: Type your username and password! </h3>
         <input type = "text" onkeypress= "hackerman()"> 
 	<button onclick= "hackerman()"> Login </button>
+	
+	<h1 id = "headingII"> Java SQL </h1>
+	<h1 class = "headingIII"> Python </h1>
+	<h1 id = "headingIIII"> HTML Javascript CSS </h1>
+	<button onclick = "testing_statement()"> In Progress: C++ Linux C# </button>
+	
   </body>
 </html>
