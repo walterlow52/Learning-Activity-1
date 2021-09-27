06adec0c520e08c55b4b84c9cb51f716a745556a
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-09-27 15:10:54 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,5 +6,8 @@
 	</script>
   </head>
 <body>
+	<h3 style = "background-color: blue;" onmouseover="hackerman()"> JS Test: Type your username and password! </h3>
+        <input type = "text" onkeypress= "hackerman()"> 
+	<button onclick= "hackerman()"> Login </button>
   </body>
 </html>
