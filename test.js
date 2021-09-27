bf116d3a457f675b6a8a3748368a1bb39f13f29f
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-09-27 16:27:37 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
   <html>
   <head>
   <script>
-		alert('HELLO USER! YOUR COMPUTER IS BEING HACKED! PLEASE HIT THE BLUE BUTTON TO CLEAN YOUR PC.')
-	</script>
+     alert('HELLO USER! YOUR COMPUTER IS BEING HACKED! PLEASE HIT THE BLUE BUTTON TO CLEAN YOUR PC.')
+  </script>
 
 <script type = "text/javascript"> 
 	function testing_statement() {
@@ -11,7 +11,25 @@
 	document.queryselector("#headingII").innerHTML = "This is a test";
 	document.queryselector(".headingIII").innerHTML = "I repeat, this is a test";
 	document.queryselector("#headingIIII").innerHTML = "<span style ='color:red'> Test test one two check check </span>"
+}
+
+<script type = "text/javascript">
+	document.addEventListener('DOMContentLoaded', function() {
+	document.queryselector("button").onclick = inc;
+}
+)
+
+let counter = 0
+function inc() {
+	counter = counter * 2;
+	document.queryselector("#double").innerHTML = `Product: ${counter}`;
 	
+if (counter >= 1048576) {
+	counter = 0
+	document.queryselector("#double").innerHTML = "That's enough. You probably don't know how much times you clicked that button."
+}
+}
+
   </head>
 <body>
 	<h3 style = "background-color: blue;" onmouseover="hackerman()"> JS Test: Type your username and password! </h3>
@@ -22,6 +40,9 @@
 	<h1 class = "headingIII"> Python </h1>
 	<h1 id = "headingIIII"> HTML Javascript CSS </h1>
 	<button onclick = "testing_statement()"> In Progress: C++ Linux C# </button>
+
+<h1 id = "double"> Product: 1 </h1> 
+<button> Click to double </button>
 	
   </body>
 </html>
