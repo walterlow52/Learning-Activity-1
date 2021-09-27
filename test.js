a204ac9e85080c6d9e537a8bbb90a2089ab7f858
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-09-27 14:54:18 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+  <script>
+		alert('HELLO USER! YOUR COMPUTER IS BEING HACKED! PLEASE HIT THE BLUE BUTTON TO CLEAN YOUR PC.')
+	</script>
+  </head>
+<body>
+  </body>
+</html>
