f7d0f7c9d0e91895a43254e44b09e568e0a4a678
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-11-05 19:11:31 -0400

    Update test.js

--- test.js
+++ test.js
@@ -145,9 +145,9 @@ function setQuestionView(appState) {
     return
   }
 
-  if (fetch_data(module) == "true_false")
+  if (fetch_data[appState.current_model] == "true_false")
     appState.current_view = "#question_view_true_false";
-  else if (fetch_data(module) == "text") {
+  else if (fetch_data[appState.current_model] == "text") {
     appState.current_view = "#question_view_text_input";
   }
 }
