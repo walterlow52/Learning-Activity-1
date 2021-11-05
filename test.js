0c7307658fb18597b593a949af2609219b316bd1
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-11-05 09:48:45 -0400

    Update test.js

--- test.js
+++ test.js
@@ -64,7 +64,7 @@ function handle_widget_event(e) {
 
         // Update State (current model + state variables)
         appState.current_question = 0;
-        appState.current_model = fetch_data.module.questions[appState.current_question].question;
+        appState.current_model = fetch_data(module.questions[appState.current_question].question);
         // process the appState, based on question type update appState.current_view
         setQuestionView(appState);
        
